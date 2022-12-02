dc3fa6c01e94a1fc26654679635a7f0c3d202ae7
Ilyos Khurozov <ilyos.developer@gmail.com>
2022-12-02 15:53:49 +0500

    install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-CUR_DIR=$(pwd)
+cur_dir=$(pwd)
 
 update() {
-  [[ -e $1 ]] && mv $1 $1.old
+  if test -e $1; then mv $1 $1.old; fi
   ln -s $2 $1
 }
 
@@ -11,17 +11,17 @@ cp_app_config() {
   if
     command -v $1 > /dev/null
   then
-    update ~/.config/$1 $CUR_DIR/.config/$1
+    update ~/.config/$1 $cur_dir/.config/$1
   else
     echo "$1 configs aren't copied as it is not installed"
   fi
 }
 
-update ~/.bashrc $CUR_DIR/.bashrc
+update ~/.bashrc $cur_dir/.bashrc
 
 for APP in alacritty flameshot lf neofetch nvim paru rofi
 do
   cp_app_config $APP
 done
 
-update ~/.config/wall.png $CUR_DIR/.config/wall.png
+update ~/.config/wall.png $cur_dir/.config/wall.png
